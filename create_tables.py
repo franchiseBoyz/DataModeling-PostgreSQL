1de61692d18060acfeed36eabf588ea5820c4cb6
troye <troyegibert@pm.me>
2022-10-16 23:04:14 +0300

    added password to create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -6,7 +6,7 @@ from sql_queries import create_table_queries, drop_table_queries
 def create_database():
 
     # Connect to the default Database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=data_eng user=postgres")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=data_eng user=franchise password='420GLOCKzone.'")
     conn.set_session(autocommit=True)
     cur = conn.cursor()
 
@@ -18,7 +18,7 @@ def create_database():
     conn.close()
 
     # Connect to the Sparkify Database
-    conn = psycopg2.connect("host=127.0.0.1  dbname=data_eng user=postgres")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=data_eng user=franchise password='420GLOCKzone.'")
     cur = conn.cursor()
 
     return cur, conn
