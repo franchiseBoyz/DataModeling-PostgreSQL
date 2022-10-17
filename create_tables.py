4967ec3b1e797fb74b06181bbb417dc7e2405ef2
troye <troyegibert@pm.me>
2022-10-18 00:05:07 +0300

    update 1

--- create_tables.py
+++ create_tables.py
@@ -18,7 +18,7 @@ def create_database():
     conn.close()
 
     # Connect to the Sparkify Database
-    conn = psycopg2.connect("host=127.0.0.1 dbname=data_eng user=franchise password='420GLOCKzone.'")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkify user=franchise password='420GLOCKzone.'")
     cur = conn.cursor()
 
     return cur, conn
